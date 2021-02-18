682112a4ed6c8266376900a6d02530111d2f5eb7
kinnji.tooyama <kinnchann0716@mail.com>
2021-02-18 21:57:42 +0900

    Add CreateServlet.java

--- src/controllers/CreateServlet.java
+++ src/controllers/CreateServlet.java
@@ -39,7 +39,6 @@ public class CreateServlet extends HttpServlet {
         String _token = request.getParameter("_token");
         if(_token != null && _token.equals(request.getSession().getId())) {
             EntityManager em = DBUtil.createEntityManager();
-            em.getTransaction().begin();
 
             Message m = new Message();
 
@@ -57,7 +56,6 @@ public class CreateServlet extends HttpServlet {
             if(errors.size() > 0) {
                 em.close();
 
-                // フォームに初期値を設定、さらにエラーメッセージを送る
                 request.setAttribute("_token", request.getSession().getId());
                 request.setAttribute("message", m);
                 request.setAttribute("errors", errors);
@@ -65,14 +63,12 @@ public class CreateServlet extends HttpServlet {
                 RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/messages/new.jsp");
                 rd.forward(request, response);
             } else {
-                // データベースに保存
                 em.getTransaction().begin();
                 em.persist(m);
                 em.getTransaction().commit();
                 request.getSession().setAttribute("flush", "登録が完了しました。");
                 em.close();
 
-                // indexのページにリダイレクト
                 response.sendRedirect(request.getContextPath() + "/index");
             }
         }
