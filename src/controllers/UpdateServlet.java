fc76fa9743f453d87a0da2ce386817ac49c094ba
kinnji.tooyama <kinnchann0716@mail.com>
2021-02-18 13:10:26 +0900

    Edit files

--- src/controllers/UpdateServlet.java
+++ src/controllers/UpdateServlet.java
@@ -1,4 +1,3 @@
-
 package controllers;
 
 import java.io.IOException;
@@ -16,32 +15,29 @@ import javax.servlet.http.HttpServletResponse;
 import models.Message;
 import models.validator.MessageValidator;
 import utils.DBUtil;
-
 /**
- * Servlet implementation class CreateServlet
+ * Servlet implementation class UpdateServlet
  */
-@WebServlet("/create")
-public class CreateServlet extends HttpServlet {
+@WebServlet("/update")
+public class UpdateServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public CreateServlet() {
+    public UpdateServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
 
-    /**
-     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-     */
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String _token = request.getParameter("_token");
         if(_token != null && _token.equals(request.getSession().getId())) {
-            EntityManager em = DBUtil.createEntityManager();
-            em.getTransaction().begin();
 
-            Message m = new Message();
+            EntityManager em = DBUtil.createEntityManager();
+
+
+            Message m = em.find(Message.class, (Integer)(request.getSession().getAttribute("message_id")));
 
             String title = request.getParameter("title");
             m.setTitle(title);
@@ -50,29 +46,28 @@ public class CreateServlet extends HttpServlet {
             m.setContent(content);
 
             Timestamp currentTime = new Timestamp(System.currentTimeMillis());
-            m.setCreated_at(currentTime);
             m.setUpdated_at(currentTime);
 
             List<String> errors = MessageValidator.validate(m);
             if(errors.size() > 0) {
                 em.close();
 
-                // フォームに初期値を設定、さらにエラーメッセージを送る
                 request.setAttribute("_token", request.getSession().getId());
                 request.setAttribute("message", m);
                 request.setAttribute("errors", errors);
 
-                RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/messages/new.jsp");
+                RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/messages/edit.jsp");
                 rd.forward(request, response);
+
             } else {
-                // データベースに保存
+
                 em.getTransaction().begin();
-                em.persist(m);
                 em.getTransaction().commit();
-                request.getSession().setAttribute("flush", "登録が完了しました。");
+                request.getSession().setAttribute("flush", "更新が完了しました。");
                 em.close();
 
-                // indexのページにリダイレクト
+                request.getSession().removeAttribute("message_id");
+
                 response.sendRedirect(request.getContextPath() + "/index");
             }
         }
