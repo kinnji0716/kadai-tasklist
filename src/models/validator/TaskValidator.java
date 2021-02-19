41bf3ceef65d8336bccc54c5a957de8a5c8f291a
kinnji.tooyama <kinnchann0716@mail.com>
2021-02-19 14:51:53 +0900

    tasklist

--- src/models/validator/TaskValidator.java
+++ src/models/validator/TaskValidator.java
@@ -10,11 +10,6 @@ public class TaskValidator {
     public static List<String> validate(Task t) {
         List<String> errors = new ArrayList<String>();
 
-        String title_error = validateTitle(t.getTitle());
-        if(!title_error.equals("")) {
-            errors.add(title_error);
-        }
-
         String content_error = validateContent(t.getContent());
         if(!content_error.equals("")) {
             errors.add(content_error);
@@ -23,15 +18,7 @@ public class TaskValidator {
         return errors;
     }
 
-    private static String validateTitle(String title) {
-        if(title == null || title.equals("")) {
-            return "タイトルを入力してください。";
-        }
-
-        return "";
-    }
-
-    private static String validateContent(String content) {
+        private static String validateContent(String content) {
         if(content == null || content.equals("")) {
             return "メッセージを入力してください。";
         }
